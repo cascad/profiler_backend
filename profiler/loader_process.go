1148ba1326c69ec10c30b6b80836d9ed922a9a36
Unknown <r.ryzhkov@kefirgames.com>
2018-09-26 18:11:49 +0300

    - fix: imports

--- profiler/loader_process.go
+++ profiler/loader_process.go
@@ -1,14 +1,14 @@
 package profiler
 
 import (
-		"fmt"
+	"log"
 	"time"
 )
 
 var fields = []string{"app_name", "process", "version", "room", "short_message", "full_message", "time"}
 
 func ProccessLoad(raw *LocalRawStorage, process *LocalProcessStorage, dbh *DBHelper) {
-	s1:= time.Now()
+	s1 := time.Now()
 	items, values := DrainMapper(fields, dbh)
 	raw.Lock()
 	raw.Items = items
@@ -19,6 +19,6 @@ func ProccessLoad(raw *LocalRawStorage, process *LocalProcessStorage, dbh *DBHel
 	process.Lock()
 	process.Data = *views
 	process.Unlock()
-	s2:= time.Now()
+	s2 := time.Now()
 	log.Println("processed", s2.Sub(s1), len(items), len(process.Data))
 }
